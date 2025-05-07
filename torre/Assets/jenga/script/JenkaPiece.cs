714e82365e631d760f3b3365ef00cbeb8d7448a7
Larasud <148461650+Larasud@users.noreply.github.com>
2025-05-07 20:21:22 -0300

    .

--- torre/Assets/jenga/script/JenkaPiece.cs
+++ torre/Assets/jenga/script/JenkaPiece.cs
@@ -89,15 +89,4 @@ public class JenkaPiece : MonoBehaviour
         }
         return transform.position;
     }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
